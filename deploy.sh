48616414068b471f338e5ec0dbfdbb6e18669415
yangyang <youngqiankun@163.com>
2018-08-31 15:41:49 +0800

    udapte

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package
-scp target/i-spring-cloud-eureka-server-1.0.0.jar ssh root@116.62.56.67:/home/icloud/build
+cp target/i-spring-cloud-eureka-server-1.0.0.jar /home/icloud/build
