4138023af862d0dc13aceccda415c40f8475d601
yangyang <youngqiankun@163.com>
2018-08-31 11:41:15 +0800

    udpate

---
+++ src/main/java/com/yytec/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.yytec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 2018/8/31 11:30
+ *
+ * @author yangqiankun
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
